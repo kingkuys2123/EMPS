5b3c408022ed9134d663b0ae38810e0b7c8f0072
kingkuys2123 <kylematthew.quitco@cit.edu>
2024-10-29 22:47:16 +0800

    Pushed backend to repo and update frontend project structure
    - Initialize and pushed backend Spring Book API files to GitHub repo:
    - Added additional dependencies: JsonWebToken (Token Based Auth) and Spring Security (Web App Security).
    - Updated the frontend project structure for easier management and improving the project structure's organization.
    - Implemented a global @CrossOrigin configuration in the Spring Boot API for frontend and backend integration.

---
+++ backend/src/main/java/com/appdev/wue/WueApplication.java
@@ -0,0 +1,13 @@
+package com.appdev.wue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WueApplication.class, args);
+	}
+
+}
