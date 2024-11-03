d7662f18d25bd6a7091fa6a2cbddf5c0246291d3
kingkuys2123 <kylematthew.quitco@cit.edu>
2024-11-03 20:08:34 +0800

    Implemented 404 Page not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage.jsx";
 import HomePage from "./components/HomePage.jsx";
+import PageNotFound from "./components/PageNotFound.jsx";
 
 function App() {
   return (
       <Routes>
         <Route>
+            <Route path="*" element={<PageNotFound/>} />
             <Route path="/" element={<LandingPage />} />
             <Route path="/home" element={<HomePage />} />
         </Route>
---
+++ frontend/src/components/PageNotFound.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Typography, Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+function PageNotFound() {
+    return (
+        <Box sx={{ backgroundColor: '#CFCFC4', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Container sx={{ textAlign: 'center', backgroundColor: '#F3F3F3', height: '750px', width: '750px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)', padding: '20px' }}>
+                <Box>
+                    <img src="/assets/images/page-404-not-found.png" alt="Not Found" style={{ maxWidth: '100%', maxHeight: '100%', marginBottom: '20px' }}/>
+                </Box>
+                <Typography variant="h6" sx={{ marginBottom: '20px', color: '#000000' }}>
+                    <span>Page does not exist...</span>
+                </Typography>
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <Button variant="contained" sx={{ borderRadius: '4px', backgroundColor: '#C63f47', color: '#FFFFFF', '&:hover': { backgroundColor: '#B0353F' }}}>
+                        <span>Return</span>
+                    </Button>
+                </Link>
+            </Container>
+        </Box>
+    );
+}
+
+export default PageNotFound;
